4d9957dccaf4b0b221351aab777e105d82848640
Ella MacPherson <ella.mm.macpherson@gmail.com>
2025-11-23 16:09:06 -0800

    Move func finished!

---
+++ room.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <map> //figure out how to use lol.
+
+
+using namespace std;
+
+
+class Room{
+ public:
+  //func to set description
+  Room(const char* inDescription);
+  //set exits with map?
+  void setExit(const char*, Room*);
+  //print when they enter such room
+  void print();
+  //map variable for each room, first var is direction of exit , second var is WHERE that exit goes too
+  map<const char*, Room*> exitMap;
+
+  struct Item{
+    char* description;
+  };
+
+ protected:
+  //location
+  char* description;
+  //vector of items in room.
+  vector<Item*> roomItems;
+};
